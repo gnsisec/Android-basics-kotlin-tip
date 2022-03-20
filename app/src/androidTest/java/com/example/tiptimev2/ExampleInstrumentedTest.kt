ff363ba02dbe63f8b0f7b0533a992ad72be4ab48
Matius Kristian <matius.kristian@vidio.com>
2022-03-20 23:50:39 +0700

    Add espresso test

--- app/src/androidTest/java/com/example/tiptimev2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptimev2/ExampleInstrumentedTest.kt
@@ -1,5 +1,14 @@
 package com.example.tiptimev2
 
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import org.hamcrest.Matchers.containsString
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,18 +16,33 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CalculatorTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tiptimev2", appContext.packageName)
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00"))
+        closeSoftKeyboard()
+        onView(withId(R.id.calculate_button)).perform(click())
+        onView(withId(R.id.tip_result)).check(matches(withText(containsString("$8.00"))))
+    }
+
+    @Test
+    fun calculate_20_percent_tip_without_rounding() {
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00"))
+        closeSoftKeyboard()
+        onView(withId(R.id.round_up_switch)).perform(click())
+        onView(withId(R.id.calculate_button)).perform(click())
+        onView(withId(R.id.tip_result)).check(matches(withText(containsString("$7.50"))))
     }
 }
